90fecbc150627b9744f4b383c019cab0de21fb7e
Lina Henze <linaaw.henze@outlook.de>
2017-08-23 20:10:00 +0200

    Farbänderung

--- ProjectTerra/MainGame.cs
+++ ProjectTerra/MainGame.cs
@@ -73,7 +73,7 @@ namespace ProjectTerra
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Azure);
 
             // TODO: Add your drawing code here
 
