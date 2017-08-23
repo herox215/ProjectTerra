843090c6089538c880bd3df7d40abacd2387f329
Lina Henze <linaaw.henze@outlook.de>
2017-08-23 21:28:58 +0200

    Zufallszahl generator hinzugefügt

---
+++ Terra.Helper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Terra.Helper.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Terra.Helper.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f437f9c-ed72-49a0-81d6-5ee25e5586c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Terra.Helper.Tests/RandomNumberTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terra.Helper.Generation;
+
+namespace Terra.Helper.Tests
+{
+    [TestClass]
+    public class RandomNumberTest
+    {
+        [TestMethod]
+        public void RandomNumberGeneratorTest()
+        {
+            int result = 0;
+        
+            RandomNumberGenerator tester = new RandomNumberGenerator();
+
+            result = tester.GenerateNumber(1, 3);
+
+            Assert.IsTrue(result < 4);
+            Assert.IsTrue(result > 0);
+        }
+    }
+}
